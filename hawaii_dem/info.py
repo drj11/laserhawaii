ea66e3c282d3ce8abc8a1424ddd9014250c4a244
David Jones <drj@pobox.com>
2013-01-14 20:45:24 +0000

    functions

--- hawaii_dem/info.py
+++ hawaii_dem/info.py
@@ -4,16 +4,29 @@
 
 import struct
 
-bytes = open('dblbnd.adf').read()
-bounds = struct.unpack('>4d', bytes)
-print "bounds (*10e-4 degrees?)", bounds
+def bounds(filename='dblbnd.adf'):
+    bytes = open(filename).read()
+    bounds = struct.unpack('>4d', bytes)
+    return bounds
 
-bytes = open('w001001x.adf').read()
-magic = bytes[0:8]
-exemplar = '0000270AFFFFFC14'.decode('hex')
-print "Magic", magic==exemplar
-size, = struct.unpack('>l', bytes[24:28])
-print "Size", size, "shorts"
-for i in range(100,size*2,8):
-  pointer = struct.unpack('>2l', bytes[i:i+8])
-  print pointer
+print "bounds (*10e-4 degrees?)", bounds()
+
+def adfIndex(filename='w001001x.adf'):
+    """Returns a list of pairs."""
+    bytes = open(filename).read()
+    magic = bytes[0:8]
+    exemplar = '0000270AFFFFFC14'.decode('hex')
+    if magic != exemplar:
+      raise BadMagic("magic %s exemplar %s" %
+        (magic.encode('hex'), exemplar.encode('hex')))
+
+    size, = struct.unpack('>l', bytes[24:28])
+
+    l = []
+    for i in range(100,size*2,8):
+      pointer = struct.unpack('>2l', bytes[i:i+8])
+      l.append(pointer)
+    return l
+
+index = adfIndex()
+print index[:10]

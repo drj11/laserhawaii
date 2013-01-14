9deb0ad283983d075984d719bdfc4b33a7e883b4
David Jones <drj@pobox.com>
2013-01-14 13:13:28 +0000

    stuff

---
+++ hawaii_dem/info.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# see
+
+import struct
+
+bytes = open('dblbnd.adf').read()
+bounds = struct.unpack('>4d', bytes)
+print "bounds (*10e-4 degrees?)", bounds
+
+bytes = open('w001001x.adf').read()
+magic = bytes[0:8]
+exemplar = '0000270AFFFFFC14'.decode('hex')
+print "Magic", magic==exemplar
+size, = struct.unpack('>l', bytes[24:28])
+print "Size", size, "shorts"
+for i in range(100,size*2,8):
+  pointer = struct.unpack('>2l', bytes[i:i+8])
+  print pointer

d75d3c94cf239f53089c92903b2a19fd516d6409
David Jones <drj@pobox.com>
2013-01-16 08:07:59 +0000

    output height in PGM

--- hawaii_dem/arctopnm.py
+++ hawaii_dem/arctopnm.py
@@ -5,6 +5,7 @@ import sys
 import arc
 
 def main(out=sys.stdout):
+    mode = 'height'
     raster = arc.Raster()
     # number of tiles in X and Y
     tx,ty = raster.header.tiletiles
@@ -12,11 +13,18 @@ def main(out=sys.stdout):
     tw,th = raster.header.tilesize
     w = tx*tw
     h = ty*th
-    out.write("P2 %d %d 1\n" % (w, h))
+    if mode == 'mask':
+        MAXVAL = 1
+    else:
+        MAXVAL = 5000
+    out.write("P2 %d %d %d\n" % (w, h, MAXVAL))
 
     for row in raster.rows():
-      b = (int(x >= 0) for x in row)
-      out.write("%s\n" % ' '.join(str(x) for x in b))
+      if mode == 'mask':
+          v = (int(x >= 0) for x in row)
+      else:
+          v = (int(round(x)) if x >= 0 else MAXVAL for x in row)
+      out.write("%s\n" % ' '.join(str(x) for x in v))
 
 if __name__ == '__main__':
    main()
